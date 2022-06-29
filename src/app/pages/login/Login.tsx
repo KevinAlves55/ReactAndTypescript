8a2e7c3ae5fc4f5f03ac4afc862ec48ee7685b27
KevinAlves55 <programador.kevin4773@gmail.com>
2022-06-29 15:24:15 -0300

    feat: add login page and form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 export const Login = () => {
 
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    
+    const handleLogin = () => {
+
+        console.log(email);
+        console.log(senha);
+        
+
+    }
+
     return(
-        <h1>TELA DE LOGIN</h1>
+        <div>
+            <h1>TELA DE LOGIN</h1>
+
+            <form>
+                <label>
+                    <span>E-mail: </span>
+                    <input value={email} type="email" onChange={e => setEmail(e.target.value)} placeholder="Digite seu E-mail"/>
+                </label>
+
+                <label>
+                    <span>Senha: </span>
+                    <input value={senha} type="password" onChange={e => setSenha(e.target.value)} 
+                    placeholder="Digite sua Senha"/>
+                </label>
+
+                <button type="button" onClick={handleLogin}>
+                    Entrar
+                </button>
+            </form>
+        </div>
     );
 
 }
